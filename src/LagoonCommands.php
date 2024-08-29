32161d496a62ebe721f10e9e388fa3229accdbd9
Blaize M Kaye <blaize.kaye@gmail.com>
2024-08-29 11:08:16 +1000

    Adds Lagoon env checks to commands

--- src/LagoonCommands.php
+++ src/LagoonCommands.php
@@ -4,6 +4,7 @@ namespace Drush\Commands\drupal_integrations;
 
 use Consolidation\SiteAlias\SiteAliasManagerAwareTrait;
 use Drush\Commands\DrushCommands;
+use Drush\Exceptions\CommandFailedException;
 use Drush\Drush;
 use Drush\Exceptions\CommandFailedException;
 use Drush\SiteAlias\SiteAliasManagerAwareInterface;
